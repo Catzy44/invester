80e73fbd06bd6da57e88a23aad64eb9d79483445
catzy44 <przemowi2002@gmail.com>
2025-04-02 07:59:53 +0200

    cleanup configuration

--- src/main/java/me/catzy/invester/InvesterApplication.java
+++ src/main/java/me/catzy/invester/InvesterApplication.java
@@ -6,18 +6,23 @@ import java.io.FileWriter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 import org.springframework.data.rest.core.config.ProjectionDefinitionConfiguration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.projection.ProjectionDefinitions;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 @EnableScheduling
+@EnableCaching
 public class InvesterApplication {
 	public static final boolean SMS_CACHE_PRELOAD_DISABLED = false;
 
@@ -35,6 +40,24 @@ public class InvesterApplication {
 		application.run(args);
 	}
 
+	public static void setCorsRegistry(CorsRegistry registry) {
+		registry.addMapping("/**")
+		.allowedOrigins(new String[] { "*" })
+		.allowedMethods(new String[] { "GET", "PUT", "DELETE", "POST", "PATCH", "OPTIONS" })
+		.allowCredentials(false)
+		.maxAge(3600L);
+	}
+	
+	@Bean
+	public RepositoryRestConfigurer repositoryRestConfigurer() {
+		return new RepositoryRestConfigurer() {
+			@Override
+			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+				InvesterApplication.setCorsRegistry(cors);
+			}
+		};
+	}
+	
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {//
@@ -43,28 +66,19 @@ public class InvesterApplication {
 			}
 		};
 	}
-
-	public static void setCorsRegistry(CorsRegistry registry) {
-		registry.addMapping("/**")
-		.allowedOrigins(new String[] { "*" })
-		.allowedMethods(new String[] { "GET", "PUT", "DELETE", "POST", "PATCH", "OPTIONS" })
-		.allowCredentials(false)
-		.maxAge(3600L);
+	
+	@Bean
+	public CacheManager cacheManager() {
+		return (CacheManager) new ConcurrentMapCacheManager(new String[] { "test" });
 	}
 
-	@Configuration
-	public class ProjectionFactoryConfig {
-		@Bean
-		public ProjectionFactory projectionFactory() {
-			return (ProjectionFactory) new SpelAwareProxyProjectionFactory();
-		}
+	@Bean
+	public ProjectionFactory projectionFactory() {
+		return (ProjectionFactory) new SpelAwareProxyProjectionFactory();
 	}
 
-	@Configuration
-	public class ProjectionDefinitionsConfig {
-		@Bean
-		public ProjectionDefinitions projectionDefinitions() {
-			return (ProjectionDefinitions) new ProjectionDefinitionConfiguration();
-		}
+	@Bean
+	public ProjectionDefinitions projectionDefinitions() {
+		return (ProjectionDefinitions) new ProjectionDefinitionConfiguration();
 	}
 }
